70e1d6e88801529f799e636552c1551289c5751c
Nikolai Alexander <njalexander93@gmail.com>
2025-02-19 15:34:21 +0000

    Fixed backend bug

--- backend/main.py
+++ backend/main.py
@@ -16,13 +16,29 @@ from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 from marty.marty import router as marty_router
 
+ENV = os.getenv("ENV", "development")
+if ENV not in ["development", "production"]:
+    raise ValueError("ENV environment variable must be either 'development' or 'production'")
+else:
+    env_file = f".env.{ENV}"
+
+load_dotenv(".env")
+if os.path.exists(env_file):
+    load_dotenv(env_file, override=True)
+if os.path.exists(".env.local"):
+    load_dotenv(".env.local", override=True)
+
 # Create the FastAPI app
 app = FastAPI(title="Marty Chatbot API", version=__version__, description="API for the Marty Chatbot")
 
 # Add CORS middleware to allow requests from the frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[os.getenv("NEXT_PUBLIC_FRONTEND_URL")],  # Allow frontend
+    allow_origins=[
+        "http://localhost:3000",     # Local development
+        "http://127.0.0.1:3000",
+        os.getenv("NEXT_PUBLIC_FRONTEND_URL")
+    ],  # Allow frontend
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
